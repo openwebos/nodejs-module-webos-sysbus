d03bb0e32973b613e37afc587ca5ae4af0e8a44f
Mark Bessey <mark.bessey@lge.com>
2013-08-15 00:57:51 -0700

    JavaScript services use less CPU
    :Release Notes:
    Fixed an issue that was causing JavaScript services to use up to 15% of the CPU when idle
    
    :Detailed Notes:
    The lifecycle handling of uv_poll_t objects in the nodejs-module-webos-sysbus add-on
    was incorrect, and therefore the event loop was basically running constantly.
    
    This *may* also address GF-13309, whoch was a crash caused by accessing freed
    memory. The old code allowed some cases where the freed handle might be accessed later.
    
    :Testing Performed:
    Tested helloworld service. Idle CPU usage is ~0% now. In addition, with verbose logging
    enabled, it's clear that the uv_poll_t objects are being allocated, started, stopped,
    and deleted at the proper times.
    
    :QA Notes:
    
    :Issues Addressed:
    [GF-12899] com.palm.service.accounts consuming 15% CPU at boot
    [GF-13309] nodejs 0.10 crashing
    
    Open-webOS-DCO-1.0-Signed-off-by: Mark Bessey <mark.bessey@lge.com>
    
    Change-Id: Ibd6a0dba33926ee4d67b61c54dca57094578925b
    Reviewed-on: https://g2g.palm.com/3097
    Reviewed-by: Build Verification
    Reviewed-by: Jason Lai <jason.lai@lge.com>
    Tested-by: Jason Lai <jason.lai@lge.com>
    Reviewed-by: Mark Bessey <mark.bessey@lge.com>
    Tested-by: Mark Bessey <mark.bessey@lge.com>

--- src/node_ls2.cpp
+++ src/node_ls2.cpp
@@ -35,18 +35,33 @@ using namespace v8;
 using namespace node;
 using namespace std;
 
+typedef std::map<int, uv_poll_t *> WatcherMap;
+typedef std::multimap<int, GPollFD *> PollfdMap;
+typedef std::map<int, int> EventMap;
+
 struct econtext {
-    GPollFD* pfd;
-    int nfd, afd;
+    GPollFD* pfd; // GpollFD objects from Glib
+    int nfd, afd; // number of GpollFD objects, allocated number of objects (rounded up to 2^n)
     gint maxpri;
 
     uv_prepare_t pw;
     uv_check_t cw;
-    uv_timer_t tw;
-    uv_poll_t* poll;
+    uv_timer_t tw;   // timeout to prevent libuv from running event loop "too soon"
 
     GMainContext* gc;
 };
+
+struct PollData {
+    int fd;
+};
+
+// a map of fds to GPollFDs
+static PollfdMap pfdMap;
+
+// a map of fds to uv_poll_t
+static WatcherMap pollwMap;
+
+#undef VERBOSE_LOGGING
 
 static uv_timer_t timeout_handle;
 static bool query = false;
@@ -62,24 +77,27 @@ static void uv_timeout_cb(uv_timer_t *handle, int status)
     uv_timer_stop(&timeout_handle);
 }
 
-// a map of fds to GPollFDs 
-std::multimap<int, GPollFD *>pfdMap;
-
 static void poll_cb(uv_poll_t* handle, int status, int events)
 {
-    GPollFD *pfd = (GPollFD *) handle->data;
-    int fd = pfd->fd;
-
+    PollData *data = (PollData *) handle->data;
+    int fd = data->fd;
+    #ifdef VERBOSE_LOGGING
+        std::cerr << "poll_cb, fd: " << fd << "events: " << events << std::endl;
+    #endif
     // Iterate over *all* GPollFDs matching the watcher's fd
-    std::pair <std::multimap<int,GPollFD *>::iterator, std::multimap<int,GPollFD *>::iterator> ret;
+    std::pair <PollfdMap::iterator, PollfdMap::iterator> ret;
     ret = pfdMap.equal_range(fd);
-    for (std::multimap<int,GPollFD *>::iterator it=ret.first; it!=ret.second; ++it) {
-        pfd = it->second;
+    for (PollfdMap::iterator it=ret.first; it!=ret.second; ++it) {
+        GPollFD *pfd = it->second;
         pfd->revents |= pfd->events & ((events & UV_READABLE ? G_IO_IN : 0) | (events & UV_WRITABLE ? G_IO_OUT : 0));
+        #ifdef VERBOSE_LOGGING
+            std::cerr << "    pfd->fd: " << pfd->fd << "pfd->events: " << pfd->events << std::endl;
+            std::cerr << "    pfd->revents: " << pfd->revents << std::endl;
+        #endif
     }
 
     pfdMap.erase(fd);
-    uv_poll_stop(handle); 
+    uv_poll_stop(handle);
 }
 
 static void prepare_cb(uv_prepare_t* w, int revents)
@@ -103,7 +121,6 @@ static void prepare_cb(uv_prepare_t* w, int revents)
                                       ctx->afd))
           ) {
         free(ctx->pfd);
-        free(ctx->poll);
 
         ctx->afd = 1;
 
@@ -112,32 +129,81 @@ static void prepare_cb(uv_prepare_t* w, int revents)
         }
 
         ctx->pfd = (GPollFD*)malloc(ctx->afd * sizeof(GPollFD));
-        ctx->poll = (uv_poll_t*) malloc(ctx->afd * sizeof(uv_poll_t));
     }
 
-    // Create poll handle and start polling
+    // store read/write flags for each FD
+    EventMap events;
+
+    // libuv does not support more than one watcher on same fd
+    // iterate through GPollFD list, accumulating read/write flags for each FD, and creating a map from fd to GpollFD
+    // for event dispatch in poll_cb()
+    pfdMap.clear();
     for (i = 0; i < ctx->nfd; ++i) {
-
         GPollFD* pfd = ctx->pfd + i;
-        uv_poll_t* pollw = ctx->poll + i;
         int fd = pfd->fd;
-
-        // libuv does not support more than one watcher on same fd
-        // Create a map of fds and check for duplicate fd
-        std::multimap<int,GPollFD *>::iterator it = pfdMap.find(fd);
-        if (it != pfdMap.end()) {
-                pfdMap.insert(std::pair<int, GPollFD *>(fd, pfd));
-                continue;
-        }
-        // insert into map
-        pfdMap.insert(std::pair<int, GPollFD *>(fd, pfd));
-        // Feed gcontext fd data to libuv and start polling
+        //translate events from Glib constants to the libuv values
+        int uv_events = (pfd->events & G_IO_IN ? UV_READABLE: 0) | (pfd->events & G_IO_OUT ? UV_WRITABLE: 0);
+        #ifdef VERBOSE_LOGGING
+            std::cerr << "fd: " << fd << ", pfd->events: " << pfd->events << ", uv_events: " << uv_events << std::endl;
+        #endif
+        //reset received events for the GPollFD
         pfd->revents = 0;
-        //TODO: Make this a list of pfd structs, maybe? Not necessary with the multimap...
-        pollw->data = pfd;
+        // Create a map of fds to GPollFD objects
+        pfdMap.insert(std::pair<int, GPollFD *>(fd, pfd));
+        EventMap::iterator it = events.find(fd);
+        if (it != events.end()) {
+            it->second |= uv_events;
+        } else {
+            events.insert(std::pair<int, int>(fd, uv_events));
+        }
+    }
+    // Create poll handles where necessary, and start polling
+    for (EventMap::iterator it = events.begin(); it != events.end(); it++) {
+        uv_poll_t *pollw;
+        std::pair<int, int> p = *it;
+        int fd = p.first;
+        int mask = p.second;
+        #ifdef VERBOSE_LOGGING
+            std::cerr << "fd: " << fd << ", mask: " << mask << std::endl;
+        #endif
+        WatcherMap::iterator pollFound = pollwMap.find(fd);
+        if (pollFound == pollwMap.end()) {
+            // not found - create a new uv_poll_t watcher, and initialize it
+            #ifdef VERBOSE_LOGGING
+                std::cerr << "creating new uv_poll_t for fd:" << fd <<std::endl;
+            #endif
+            pollw = new uv_poll_t;
+            PollData *pd = new PollData;
+            pd->fd = fd;
+            pollw->data = pd;
+            pollwMap.insert(std::pair<int, uv_poll_t*>(fd, pollw));
+            uv_poll_init(uv_default_loop(), pollw, fd);
+        } else {
+            // reuse existing watcher
+            std::pair<int, uv_poll_t *> p = *pollFound;
+            pollw = p.second;
+        }
+        // Feed fd data to libuv and start polling
+        // This will reset the mask if pollw is already registered
+        uv_poll_start(pollw, mask, poll_cb);
+    }
 
-        uv_poll_init(uv_default_loop(), pollw, pfd->fd);	
-        uv_poll_start(pollw, UV_READABLE | UV_WRITABLE, poll_cb);
+    // remove watchers that are no longer needed
+    for (WatcherMap::iterator it = pollwMap.begin(); it != pollwMap.end(); it++) {
+        std::pair<int, uv_poll_t *> p = *it;
+        int fd = p.first;
+        uv_poll_t *pollw = p.second;
+        EventMap::iterator eventFound = events.find(fd);
+        if (eventFound == events.end()) {
+            #ifdef VERBOSE_LOGGING
+            std::cerr << "removing uv_poll_t for fd:" << fd <<std::endl;
+            #endif
+            uv_poll_stop(pollw);
+            pollwMap.erase(fd);
+            PollData *data = (PollData *)pollw->data;
+            delete data;
+            delete pollw;
+        }
     }
 
     if (timeout >= 0) {
@@ -166,7 +232,7 @@ static void check_cb(uv_check_t* w, int revents)
 
 static struct econtext default_context;
 
-init(Handle<Object> target)
+void init(Handle<Object> target)
 {
     HandleScope scope;
     gMainLoop = g_main_loop_new(NULL, true);
@@ -187,7 +253,7 @@ init(Handle<Object> target)
     uv_unref((uv_handle_t*) &ctx->pw);
 
     uv_check_init(uv_default_loop(), &ctx->cw);
-    uv_check_start (&ctx->cw, check_cb); 
+    uv_check_start (&ctx->cw, check_cb);
     uv_unref((uv_handle_t*) &ctx->cw);
 
     // Timer
